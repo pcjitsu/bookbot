b0f19a444b4459f958e0a72b5c7540ebf250a13c
tony <45546763+pcjitsu@users.noreply.github.com>
2024-12-16 19:26:05 -0600

    B: set up Char counter

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def count(file):
     count = len(words)
     print(count)
 
+def char_count(file):
+    low_char = file.lower()
+    char_dict = {}
+    for char in low_char:
+        if(char not in char_dict):
+            char_dict[char] = 0
+        if(char in char_dict):
+            char_dict[char] = char_dict[char] + 1
+    print(char_dict)
+
+
+
+
 file_contents = main()
 
 count(file_contents)
+char_count(file_contents)
